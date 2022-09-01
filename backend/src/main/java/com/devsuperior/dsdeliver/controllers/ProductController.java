13be33be16a246160a25d3efd5b0ac459adce5f4
Luan Rafael <luanraffaell@hotmail.com>
2022-09-01 13:10:46 -0300

    GET/products

---
+++ backend/src/main/java/com/devsuperior/dsdeliver/controllers/ProductController.java
@@ -0,0 +1,27 @@
+package com.devsuperior.dsdeliver.controllers;
+
+import java.util.List;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.devsuperior.dsdeliver.dto.ProductDTO;
+import com.devsuperior.dsdeliver.service.ProductService;
+
+@RestController
+@RequestMapping("/products")
+public class ProductController {
+	private ProductService productService;
+	
+	public ProductController(ProductService productService) {
+		this.productService = productService;
+	}
+	
+	@GetMapping
+	public ResponseEntity<List<ProductDTO>> findAll(){
+		List<ProductDTO> list = productService.findAll();
+		return ResponseEntity.ok().body(list);
+	}
+}
